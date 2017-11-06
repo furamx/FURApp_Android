1de64a311ba5c2e5fcf0a3ba1d61370417178561
Eduardo Aguilera <laloao.2302@gmail.com>
2017-11-06 11:28:52 -0600

    Add signup activity

---
+++ app/src/main/java/fura/com/furapp_android/view/SignUp.java
@@ -0,0 +1,12 @@
+package fura.com.furapp_android.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
