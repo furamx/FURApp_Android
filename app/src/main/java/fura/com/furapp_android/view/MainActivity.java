d822b149c3323c6ca9ae442cfc3629aa8305e542
Eduardo Aguilera <laloao.2302@gmail.com>
2017-11-06 12:46:04 -0600

    Email registration and login works

--- app/src/main/java/fura/com/furapp_android/view/MainActivity.java
+++ app/src/main/java/fura/com/furapp_android/view/MainActivity.java
@@ -3,6 +3,8 @@ package fura.com.furapp_android.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import fura.com.furapp_android.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+        } else {
+            // not signed in
+        }
+
         setContentView(R.layout.activity_main);
     }
 }
