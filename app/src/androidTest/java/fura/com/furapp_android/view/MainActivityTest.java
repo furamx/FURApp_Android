9dea1d1c66a8de4a6b9812257d0bb4a106ef405b
Daniela Ramon <ramon@MacBook-Pro-de-Daniela.local>
2017-11-08 11:49:44 -0600

    Bottom menu and navigation test

---
+++ app/src/androidTest/java/fura/com/furapp_android/view/MainActivityTest.java
@@ -0,0 +1,40 @@
+package fura.com.furapp_android.view;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import fura.com.furapp_android.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+/**
+ * Created by ramon on 06/11/17.
+ */
+public class MainActivityTest {
+    @Rule
+    public IntentsTestRule<MainActivity> MainActivityRule=new IntentsTestRule<>(MainActivity.class);
+    @Before
+    public void SetUp(){
+        MainActivityRule.getActivity();
+    }
+    @Test
+    public void TestButtonGoToSignInActivity(){
+        onView(withId(R.id.btn_access_sign_in)).perform(click());
+        intended(hasComponent(SignInActivity.class.getName()));
+    }
+    @Test
+    public void TestButonLogInIsDisplayed(){
+        onView(withId(R.id.btn_access_sign_in)).check(matches((isDisplayed())));
+    }
+}
--- app/src/main/java/fura/com/furapp_android/view/MainActivity.java
+++ app/src/main/java/fura/com/furapp_android/view/MainActivity.java
@@ -1,5 +1,7 @@
 package fura.com.furapp_android.view;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,7 +21,12 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // not signed in
         }
-
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        BottomMenuFragment bottomMenuFragment = new BottomMenuFragment();
+        fragmentTransaction.replace(R.id.fragment_bottom_info, bottomMenuFragment, "sign_in");
+        fragmentTransaction.commit();
     }
 }
